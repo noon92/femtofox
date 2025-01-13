c72a036592aa809fbb2469ab9e08ecc26d4230d6
nagumii <nagumi@gmail.com>
2025-01-13 20:26:36 +0200

    fix pinouts

--- foxbuntu/sysdrv/out/rootfs_uclibc_rv1106/usr/local/bin/femto-pinout.sh
+++ foxbuntu/sysdrv/out/rootfs_uclibc_rv1106/usr/local/bin/femto-pinout.sh
@@ -16,84 +16,47 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
-# luckfox="\
-# |Pin #|Pin ID |Function      | Luckfox |Pin #|Pin ID |Function      |\n\
-# |-----|-------|--------------|---------|-----|-------|--------------|\n\
-# |1    |VBus   |5V in/out     |   USB   |22   |1V8    |1.8V out      |\n\
-# |2    |GND    |              |         |21   |GND    |              |\n\
-# |3    |3V3    |3.3V out      |         |20   |4C1    |1v8 IO, SARADC|\n\
-# |4/42 |1B2    |Debug UART2-TX|         |19   |4C0    |1v8 IO, SARADC|\n\
-# |5/43 |1B3    |Debug UART2-RX|     [b] |18/4 |0A4    |3v3 IO        |\n\
-# |6/48 |1C0    |CS0, IO       |         |17/55|1C7    |IRQ, IO       |\n\
-# |7/49 |1C1    |CLK, IO       |         |16/54|1C6    |BUSY, IO      |\n\
-# |8/50 |1C2    |MOSI, IO      |         |15/59|1D3    |i2c SCL       |\n\
-# |9/51 |1C3    |MISO, IO      |         |14/58|1D2    |i2c SDA       |\n\
-# |10/52|1C4    |UART4-TX      |         |13/57|1D1    |UART3-RX, NRST|\n\
-# |11/53|1C5    |UART4-RX      |   ETH   |12/56|1D0    |UART3-TX, RXEN|\n\
-# |-----|-------|--------------|---------|-----|-------|--------------|\n\
-#                               R R G T T\n\
-#                               X X N X X\n\
-#                               - + D - +"
-
-luckfox="\
-                    ┌────┬───────┬────┐                      \n\
-       VBUS 3.3-5V ●│●   │ USB-C │   ●│● 1V8 OUT             \n\
-               GND ●│●   │       │   ●│● GND                 \n\
-        3V3 IN/OUT ●│●   └───────┘   ●│● 145 (1.8V)          \n\
-UART2-TX DEBUG, 42 ●│●               ●│● 144 (1.8V)          \n\
-UART2-RX DEBUG, 43 ●│●        [BTN]  ●│● 4                   \n\
-           CS0, 48 ●│●               ●│● 55, IRQ             \n\
-           CLK, 49 ●│●               ●│● 54, BUSY            \n\
-          MOSI, 50 ●│●               ●│● 59, I2C SCL         \n\
-          MISO, 51 ●│●               ●│● 58, I2C SDA         \n\
-      UART4-RX, 52 ●│●               ●│● 57, NRST, UART3-RX  \n\
-      UART4-TX, 53 ●│●      ETH      ●│● 56, RXEN, UART3-TX  \n\
-                    └──●──●──●──●──●──┘                      \n\
-                       R  R  G  T  T                         \n\
-                       X  X  N  X  X                         \n\
-                       -  +  D  -  +                         \n\
-GPIO BANK 0 (3.3v): 4                                        \n\
-GPIO BANK 1 (3.3v): 42 43 48 49 50 51 52 53 54 55 56 57 58 59\n\
-GPIO BANK 4 (1.8v): 144 145                                  "
 
 femtofox="\
-┌──────────┬────┬─────┬────┬─────────────────┬───┬───────────────┐\n\
-│⚪:KILL  ●│●   │USB-C│   ●│●         PWR-IN │✚ ▬│ 3.3-5V      ⚪│\n\
-├───────┐ ●│●   └─────┘   ●│●                └───┘               │\n\
-│ USB-C │ ●│●             ●│●    ┌─────────────────────────────┐ │\n\
-│ POWER │ ●│●   LUCKFOX   ●│●    │       ┌─────────────┐       │ │\n\
-├───────┘ ●│●  PICO MINI  ●│●    │       │             │       │ │\n\
-├───┐     ●│●             ●│●    │   E   │             │       │ │\n\
-│ ● │GND  ●│●   FOXHOLE   ●│●    │   2   │             │       │ │\n\
-│ ● │3V3  ●│●             ●│●    │   2   │ E22-900M22S │       │ │\n\
-│ ● │TX4  ●│●             ●│●    │   |   │             │       │ │\n\
-│ ● │RX4  ●│●             ●│●    │   9   │             │       │ │\n\
-├───┘     ●│●             ●│●    │   0   └─────────────┘       │ │\n\
-│⚪        └───●─●─●─●─●───┘     │   0    ┌───────────┐        │ │\n\
-├──────────────────┐             │   M    │           │        │ │\n\
-│ ● RX-            │  I2C GROVE  │   3    │   SEEED   │        │ │\n\
-│ ● RX+            │  ┌───────┐  │   0    │WIO  SX1262│        │ │\n\
-│ ● GND  ETHERNET  │  │● ● ● ●│  │   S    │           │        │ │\n\
-│ ● TX-            │  ├───────┤  │        └───────────┘        │ │\n\
-│ ● TX+            │  │● ● ● ●│  │                             │ │\n\
-├──────────────────┘  └───────┘  └─────────────────────────────┘ │\n\
-│   ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●   │\n\
-│⚪ ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ● ⚪│\n\
-└────────────────────────────────────────────────────────────────┘\n\
-                                                                  \n\
-                R              M  M                               \n\
-    G           X        G  C  O  I  3           G     S  S  3    \n\
-    N           E        N  L  S  S  V           N     C  D  V    \n\
-    D           N        D  K  I  O  3           D     L  A  3    \n\
-    ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●    \n\
-    ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●    \n\
-    C  B  I  G     G              G        G  R  R  T  G  5  5    \n\
-    S  U  R  N     N              N        N  S  X  X  N  V  V    \n\
-    0  S  Q  D     D              D        D  T  4  4  D          \n\
-       Y                                                          \n\
-ꙩ︎ pins available on Femtofox Pro. Unused on Femtofox community.   \n\
-  KILL pins available on Femtofox Pro only. Add power switch or   \n\
-  thermal fuse/cutoff here and remove resistor.                   "
+┌──────────┬────┬─────┬────┬───────────────┬───┬───────────────┐\n\
+│⚪:⛊KILL ●│●   │USB-C│   ●│●       PWR-IN │✚ ▬│ 3.3-5V      ⚪│\n\
+├───────┐ ●│●   └─────┘   ●│●              └───┘               │\n\
+│ USB-C │ ●│●             ●│●  ┌─────────────────────────────┐ │\n\
+│ PWR ⛊ │ ●│●   LUCKFOX   ●│●  │       ┌─────────────┐       │ │\n\
+│ DEBUG │ ●│●  PICO MINI  ●│●  │       │             │       │ │\n\
+├───────┘ ●│●             ●│●  │       │             │       │ │\n\
+├───┐     ●│●   FOXHOLE   ●│●  │   E   │             │       │ │\n\
+│ ● │GND  ●│●             ●│●  │   2   │ E22-900M22S │       │ │\n\
+│ ● │3V3  ●│●             ●│●  │   2   │             │       │ │\n\
+│ ● │TX4  ●│●             ●│●  │   |   │             │       │ │\n\
+│ ● │RX4  ●│●             ●│●  │   9   │             │       │ │\n\
+├───┘      └───●─●─●─●─●───┘   │   0   └─────────────┘       │ │\n\
+│⚪                            │   0    ┌───────────┐        │ │\n\
+├──────────────────┐           │   M    │           │        │ │\n\
+│ ● RX-            │ I2C GROVE │   3    │   SEEED   │        │ │\n\
+│ ● RX+            │ ┌───────┐ │   0    │WIO  SX1262│        │ │\n\
+│ ● GND  ETHERNET  │ │● ● ● ●│ │   S    │           │        │ │\n\
+│ ● TX-            │ ├───────┤ │        └───────────┘        │ │\n\
+│ ● TX+            │ │● ● ● ●│ │                             │ │\n\
+├──────────────────┘ └───────┘ └─────────────────────────────┘ │\n\
+│  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ■  │\n\
+│⚪●  ●  ●  ●  ●  ●  ⛊  ⛊  ⛊  ●  ⛊  ⛊  ●  ●  ●  ●  ●  ●  ●  ●⚪│\n\
+└──────────────────────────────────────────────────────────────┘\n\
+                                                                \n\
+               R              M  M                              \n\
+   G           X        G  C  I  O  3           G     S  S  3   \n\
+   N           E        N  L  S  S  V           N     C  D  V   \n\
+   D           N        D  K  O  I  3           D     L  A  3   \n\
+   ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ●  ■   \n\
+   ●  ●  ●  ●  ●  ●  ⛊  ⛊  ⛊  ●  ●  ⛊  ⛊  ●  ●  ●  ●  ●  ●  ●   \n\
+   C  B  I  G  G  G  G  S  S     G  R  T  G  R  R  T  G  5  5   \n\
+   S  U  R  N  P  N  P  R  R     N  X  X  N  S  X  X  N  V  V   \n\
+   0  S  Q  D  I  D  I  A  A     D  2  2  D  T  4  4  D         \n\
+      Y        O     O  1  0                                    \n\
+⛊ Denotes Pro features:                                        \n\
+On Community Edition, PWR/DEBUG USB-C replaced by 4pin UART2.   \n\
+PWR/DEBUG USB-C also carries serial with no added adapter.      \n\
+KILL: add PWR switch or thermal cutoff/fuse & remove resistor.  "
 
 femtofox_zero="\
          ┌────┐               ┌────┐         \n\
@@ -129,6 +92,26 @@ UART4-TX │ ● │ │  │  WIO  │  │ │ ● │ I2C SCL \n\
 
 femtofox_tiny="coming soon"
 
+luckfox="\
+                    ┌────┬───────┬────┐                      \n\
+       VBUS 3.3-5V ●│●   │ USB-C │   ●│● 1V8 OUT             \n\
+               GND ●│●   │       │   ●│● GND                 \n\
+        3V3 IN/OUT ●│●   └───────┘   ●│● 145, SARADC-IN1 1.8V\n\
+UART2-TX DEBUG, 42 ●│●               ●│● 144, SARADC-IN0 1.8V\n\
+UART2-RX DEBUG, 43 ●│●        [BTN]  ●│● 4                   \n\
+           CS0, 48 ●│●               ●│● 55, IRQ             \n\
+           CLK, 49 ●│●               ●│● 54, BUSY            \n\
+          MOSI, 50 ●│●               ●│● 59, I2C SCL         \n\
+          MISO, 51 ●│●               ●│● 58, I2C SDA         \n\
+      UART4-RX, 52 ●│●               ●│● 57, NRST, UART3-RX  \n\
+      UART4-TX, 53 ●│●      ETH      ●│● 56, RXEN, UART3-TX  \n\
+                    └──●──●──●──●──●──┘                      \n\
+                       R  R  G  T  T                         \n\
+                       X  X  N  X  X                         \n\
+                       -  +  D  -  +                         \n\
+GPIO BANK 0 (3.3v): 4                                        \n\
+GPIO BANK 1 (3.3v): 42 43 48 49 50 51 52 53 54 55 56 57 58 59\n\
+GPIO BANK 4 (1.8v): 144 145                                  "
 
 # Parse options
 while getopts "hlfzt" opt; do
