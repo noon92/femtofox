bb99382e78cc766f87137397450be7dcf9af4dd6
nagumii <nagumi@gmail.com>
2025-01-17 18:07:26 +0200

    Complete initial sysinfo release

--- foxbuntu/sysdrv/out/rootfs_uclibc_rv1106/usr/local/bin/femto-meshtasticd-config.sh
+++ foxbuntu/sysdrv/out/rootfs_uclibc_rv1106/usr/local/bin/femto-meshtasticd-config.sh
@@ -77,18 +77,29 @@ while getopts ":higkl:q:va:cpo:sStruxm" opt; do
       ;;
     i) # Option -i (Get important node info)
       output=$(meshtastic --host --info)
+      preset_or_settings="Use preset?       $(echo "$output" | grep -oP '"usePreset":\s*\K\w+')\n"
+      if echo "$output" | grep -qP '"usePreset":\s*true'; then
+        preset_or_settings+="Preset:           $(echo "$output" | grep -oP '"modemPreset":\s*"\K[^"]+')"
+      else
+        preset_or_settings+="\
+Bandwidth:        $(echo "$output" | grep -oP '"bandwidth":\s*\K\w+')\n\
+Spread factor:    $(echo "$output" | grep -oP '"spreadFactor":\s*\K\w+')\n\
+Coding rate:      $(echo "$output" | grep -oP '"codingRate":\s*\K\w+')"
+      fi
       echo -e "\
 Version:          $(echo "$output" | grep -oP '"firmwareVersion":\s*"\K[^"]+' | head -n 1)\n\
 Node name:        $(echo "$output" | grep -oP 'Owner:\s*\K.*' | head -n 1)\n\
-NodeID:           $(printf "%08x\n" $(echo "$output" | grep -oP '"myNodeNum":\s*\K\d+' | head -n 1)) ($(echo "$output" | grep -oP '"myNodeNum":\s*\K\d+' | head -n 1))\n\
+NodeID:           !$(printf "%08x\n" $(echo "$output" | grep -oP '"myNodeNum":\s*\K\d+' | head -n 1))   (nodenum: $(echo "$output" | grep -oP '"myNodeNum":\s*\K\d+' | head -n 1))\n\
 TX enabled?       $(txEnabled=$(echo "$output" | grep -oP '"txEnabled":\s*\K\w+'); [[ "$txEnabled" == "true" ]] && echo "\033[0;34menabled\033[0m" || echo "\033[0;31mdisabled\033[0m")\n\
 Role:             $(echo "$output" | grep -oP '"role":\s*"\K[^"]+' | head -n 1)\n\
-Preset:           $(echo "$output" | grep -oP '"modemPreset":\s*"\K[^"]+')\n\
+$preset_or_settings\n\
+Frequency offset: $(echo "$output" | grep -oP '"bandwidth":\s*\K\w+')
 Region:           $(echo "$output" | grep -oP '"region":\s*"\K[^"]+')\n\
-$(channelNum=$(echo "$output" | grep -oP '"channelNum":\s*\K\d+' | head -n 1); [[ -n "$channelNum" && "$channelNum" -gt 0 ]] && echo -e "Frequency slot:   $channelNum")
-$(overrideFrequency=$(echo "$output" | grep -oP '"overrideFrequency":\s*\K[0-9.]+'); [[ -n "$overrideFrequency" && $(echo "$overrideFrequency > 0" | bc) -eq 1 ]] && echo -e "Override freq:    $overrideFrequency")\n\
-Uptime:           $(echo "$output" | grep -oP '"uptimeSeconds":\s*\K\d+' | head -n 1 | awk '{days=int($1/86400); hrs=int(($1%86400)/3600); mins=int(($1%3600)/60); secs=int($1%60); printf "%d days %02d:%02d:%02d\n", days, hrs, mins, secs}')\n\
-Public key:       $(echo "$output" | grep -oP '"publicKey":\s*"\K[^"]+' | head -n 1)\
+Hop limit:        $(echo "$output" | grep -oP '"hopLimit":\s*\K\w+')\n\
+$(channelNum=$(echo "$output" | grep -oP '"channelNum":\s*\K\d+' | head -n 1); [[ -n "$channelNum" && "$channelNum" -gt 0 ]] && echo -e "Frequency slot:   $channelNum\n")\
+$(overrideFrequency=$(echo "$output" | grep -oP '"overrideFrequency":\s*\K[0-9.]+'); [[ -n "$overrideFrequency" && $(echo "$overrideFrequency > 0" | bc) -eq 1 ]] && echo "Override freq:    $overrideFrequency\n")\
+Public key:       $(echo "$output" | grep -oP '"publicKey":\s*"\K[^"]+' | head -n 1)\n\
+Nodes in nodedb:  $(echo "$output" | grep -oP '"![a-zA-Z0-9]+":\s*\{' | wc -l)\
 "
       ;;
     g) # Option -g (get config URL)
@@ -145,13 +156,13 @@ Public key:       $(echo "$output" | grep -oP '"publicKey":\s*"\K[^"]+' | head -
     r) # Option -r (mesh connectivity test)
       for ((i=0; i<=2; i++)); do
         if meshtastic --host --ch-index 0 --sendtext "test" --ack 2>/dev/null | grep -q "ACK"; then
-          echo "Received acknowledgement!"
+          echo -e "Received acknowledgement...\n\n\033[0;34mMesh connectivity confirmed!\033[0m"
           exit 0
         else
-          echo "Retrying... ($((i + 1)))"
+          echo "No response, retrying... ($((i + 1)))"
         fi
       done
-      echo "Failed after 3 attempts."
+      echo -e "\033[0;31mFailed after 3 attempts.\033[0m"
       ;;
     s) # Option -s (start/restart Meshtasticd service)
       systemctl restart meshtasticd
